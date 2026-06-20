ad096e42fd9fdfe2f4354ec5ab482d658eb74e65
erfan-code-ir <erfan.sadeghi.niaraki@gmail.com>
2026-06-20 15:10:50 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import re
+def validate_email(email):
+    pattern=r"^[a-zA-Z0-9_.]+@[a-zA-Z0-9]+\.[a-zA-Z]{3}$"
+    if re.fullmatch(pattern,email):
+        return True
+    return False
+def validate_phone(number):
+    conditions=[(11,"09"),(13,"+989"),(14,"00989")]
+    for length,prefix in conditions:
+      if len(number)==length and number.startswith(prefix):
+        return True
+      return False
+print("salam,khosh amadin")
+user_email = input()
+user_phone = input()
+print(f"{validate_email(user_email)}")
+print(f"{validate_phone(user_phone)}")
